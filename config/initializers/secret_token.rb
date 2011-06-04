9a1ffc3d564c6db36065f1352519a5f562b847f7
Bruno <bruno@Brunos-MacBook.local>
2011-06-04 16:07:48 -0300

    new Rails app generated by rails_apps_composer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealWitMe::Application.config.secret_token = 'ff37b3eea7ecad71a843d9c3d8438d6d890d5cfdf674f11f6431e51dab9402016bcdf84cf9890c6009c884a7e3823fad30281f7c36e77169d71766e75049b192'
