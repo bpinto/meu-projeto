27bf490a370002342bf8f7520526e0a7ad98baec
jplethier <jplethier@gmail.com>
2011-06-08 21:53:31 -0300

    criado o form para criaçao de oferta, e o options para o select de tipo de oferta no helper de oferta O TESTE DO CUCUMBER NEW_DEAL NÃO ESTÁ PASSANDO

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,2 +1,9 @@
+# -*- coding: utf-8 -*-
 module DealsHelper
+
+  def options_for_type(selected)
+    options = ["","Bebidas", "Beleza e Saúde", "Celulares e Telefones", "DVDs e CDs", "Eletrodomésticos", "Eletrônicos", "Esportes e Lazer", "Informática", "Livros", "Roupas e Calçados", "Viagens"]
+    options_for_select(options, selected)
+  end
+
 end
