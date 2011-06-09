a34d2cf46644d1a6f6fbd3b5476c84eaa24da0aa
jplethier <jplethier@gmail.com>
2011-06-08 22:40:05 -0300

    correçao do form de DEAL e criaçao do método para criaçao da oferta

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -5,6 +5,13 @@ class DealsController < ApplicationController
   end
 
   def create
+    @deal  = current_user.deals.build(params[:deal])
+    if @deal.save
+      flash[:success] = "Oferta cadastrada com sucesso!"
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
 end
