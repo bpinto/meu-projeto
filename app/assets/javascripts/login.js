a60565b198ba85d95b5b2784fe7572d48fa1936e
jplethier <jplethier@gmail.com>
2011-07-28 10:02:36 -0300

    adaptacao para usar o heroku

--- app/assets/javascripts/login.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#login-link').click(function(event){
-    event.preventDefault(); // Prevent link from following its href
-    $("#box-login").animate({"height": "toggle"}, { duration: 200 });
-  });
-});
