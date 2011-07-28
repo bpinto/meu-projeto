9d12da834b492df31222deced5886b2945e63486
jplethier <jplethier@gmail.com>
2011-07-28 10:40:37 -0300

    deletando código antigo

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-# By using the symbol ':user', we get Factory Girl to simulate the User model.
-Factory.define :user do |user|
-  user.name                  "teste"
-  user.email                 "teste@teste.com"
-  user.password              "123456"
-  user.password_confirmation "123456"
-end
-
--- spec/models/oferta_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Oferta do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
