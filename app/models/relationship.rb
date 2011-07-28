0fad7fc642e87e97446cbe15202619a081a54ce2
jplethier <jplethier@gmail.com>
2011-07-28 10:03:08 -0300

    adaptacao para usar o heroku

--- app/models/relationship.rb
+++
@@ -1,9 +0,0 @@
-require 'not_equal_validator'
-
-class Relationship < ActiveRecord::Base
-  belongs_to :follower, :class_name => "User"
-  belongs_to :followed, :class_name => "User"
-
-  validates :followed_id, :presence => true, :uniqueness => { :scope => :follower_id }
-  validates :follower_id, :presence => true, :not_equal => { :with => :followed_id, :message => "You cannot follow yourself" }
-end
--- app/validators/greater_than_validator.rb
+++
@@ -1,7 +0,0 @@
-class GreaterThanValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if (value <= record.send(options[:with]))
-      record.errors[attribute] << (options[:message] || "is not greater than #{options[:with]}")
-    end
-  end
-end
