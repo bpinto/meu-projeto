69ee263767b71c34a7890cd07708ac297649ddb7
Bruno Ferreira Pinto <brunoferreirapinto@gmail.com>
2011-08-21 11:15:49 -0300

    Adicionado o layout de exibicao de uma oferta

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,5 +1,5 @@
 class DealsController < AuthorizedController
-  skip_before_filter :authenticate_user!, :only => :today
+  skip_before_filter :authenticate_user!, :only => [:show, :today]
   prepend_before_filter :find_todays_deals, :only => :today
 
   def new
@@ -14,6 +14,9 @@ class DealsController < AuthorizedController
       flash.now[:error] = "Foram encontrados erros ao criar a oferta."
       render :new
     end
+  end
+
+  def show
   end
 
   def today
