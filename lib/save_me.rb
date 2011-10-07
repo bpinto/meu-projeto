a2c568c865cd55b8b16d0a9af3fe32e5900b97e3
Bruno Ferreira Pinto <brunoferreirapinto@gmail.com>
2011-10-07 14:14:02 -0300

    Crawler do save_me criado.

---
+++ lib/save_me.rb
@@ -0,0 +1,85 @@
+# encoding: UTF-8
+# require 'anemone'
+require 'nokogiri'
+require 'open-uri'
+require 'pry' #Debugger
+
+class SaveMe
+  attr_reader :doc
+
+  XPATH_LINKS = '//div[@class="offer"]/div[@class="offer_text"]/a'
+  XPATH_ADDRESS = '//p[@class="endereco_local"]'
+  XPATH_COMPANY = '//p[@class="nome_local"]'
+  XPATH_DESCRIPTION = '//div[@class="detalhes_middle"]//p'
+  XPATH_LINK = '//div[@class="ir-para-site"]/a'
+  XPATH_PRICE = '//p[@class="preco-atual"]/span[@class="preco"]'
+  XPATH_REAL_PRICE = '//p[@class="preco-antigo"]/span[@class="preco"]'
+  XPATH_TITLE = '//div[@class="oferta-imagem"]//a'
+
+  URL = "http://www.saveme.com.br"
+  DAILY_DEALS = "/compra-coletiva/"
+
+  CATEGORIES = {
+    "bares-e-baladas" => Deal::CATEGORY_DRINK,
+    "cursos-e-aulas" => Deal::CATEGORY_BOOK,
+    "entretenimento" => Deal::CATEGORY_FITNESS,
+    "esporte" => Deal::CATEGORY_FITNESS,
+    "hoteis-e-viagens" => Deal::CATEGORY_TRAVEL,
+    "produtos" => nil,
+    "restaurantes" => Deal::CATEGORY_RESTAURANT,
+    "saude-e-beleza" => Deal::CATEGORY_BEAUTY_AND_HEALTH,
+    "servicos-locais" => nil
+  }
+
+  def initialize
+    html = open("#{URL}#{DAILY_DEALS}rio-de-janeiro/")
+    @doc = Nokogiri::HTML(html.read)
+    @doc.encoding = 'utf-8'
+  end
+
+  def try_catch(&block)
+    begin
+      block.call
+    rescue Exception => ex
+      #TODO: LOGGER
+      nil
+    end
+  end
+
+  def deals
+    city_id = City.find_by_name("Rio de Janeiro").id
+
+    links_to_crawl.each do |link|
+      deal_hash = {}
+
+      html = open("#{URL}#{link}")
+      page = Nokogiri::HTML(html.read)
+      page.encoding = 'utf-8'
+
+      deal_hash[:address] = try_catch { page.xpath(XPATH_ADDRESS).children.first.text.scan(/.+\n/).first.strip }
+
+      category = try_catch { link.scan(/rio-de-janeiro\/(.*)\//).first.first }
+      deal_hash[:category] = CATEGORIES[category]
+
+      deal_hash[:city_id] = city_id
+      deal_hash[:company] = try_catch { page.xpath(XPATH_COMPANY).text }
+      deal_hash[:description] = try_catch { page.xpath(XPATH_DESCRIPTION).first.text.strip }
+
+      contador = try_catch { page.css('script').grep(/atualizaContador/).first.children.text.scan(/\((.*)\)/).first.first.split(',') }
+      deal_hash[:end_date] = contador ? (Time.new contador[0], contador[1], contador[2], contador[3], contador[4], contador[5].to_i) : nil
+
+      deal_hash[:kind] = try_catch { Deal::KIND_DAILY_DEAL }
+      deal_hash[:link] = try_catch { page.xpath(XPATH_LINK).first[:href].strip }
+      deal_hash[:price_mask] = try_catch { page.xpath(XPATH_PRICE).first.text.strip }
+      deal_hash[:real_price_mask] = try_catch { page.xpath(XPATH_REAL_PRICE).first.text.strip }
+      deal_hash[:title] = try_catch { page.xpath(XPATH_TITLE).text }
+
+      deal = Deal.new deal_hash
+      binding.pry
+    end
+  end
+
+  def links_to_crawl
+    @doc.xpath(XPATH_LINKS).collect { |link| link[:href] }
+  end
+end
---
+++ spec/save_me_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require './lib/save_me.rb'
+
+describe SaveMe do
+  let(:saveme) { SaveMe.new }
+  subject { saveme }
+
+  describe "#deals" do
+    it "a" do
+      City.create name: "Rio de Janeiro", state: "Rio de Janeiro", country: "Brasil"
+      saveme.deals
+    end
+  end
+end
