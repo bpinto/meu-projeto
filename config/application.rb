9327dd71ce893e157164718e354e7dd2e3552cbd
Bruno Ferreira Pinto <brunoferreirapinto@gmail.com>
2011-10-07 14:38:12 -0300

    Assets precisam ser pre-compilados em producao

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,15 @@
 require File.expand_path('../boot', __FILE__)
-
-# Pick the frameworks you want:
 require 'rails/all'
-# require "active_record/railtie"
-# require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "active_resource/railtie"
-# require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  # Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module DealWitMe
   class Application < Rails::Application
-
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
       g.view_specs false
