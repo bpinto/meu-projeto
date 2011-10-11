544c94ea1942f66706474967c9965377ee06007f
Bruno Ferreira Pinto <brunoferreirapinto@gmail.com>
2011-10-10 23:04:59 -0300

    Configuracoes de desenvolvimento e producao dos assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,15 @@ DealWitMe::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
   # Compress both stylesheets and JavaScripts
   config.assets.js_compressor  = :uglifier
   config.assets.css_compressor = :scss
