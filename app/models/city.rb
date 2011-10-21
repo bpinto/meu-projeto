ea72bf340217de2a3995a8ac4a4cd55932216332
jplethier <jplethier@gmail.com>
2011-10-21 14:47:18 -0200

    oferta nacional vindo no início da lista de cidades para o cadastro de oferta

--- app/models/city.rb
+++ app/models/city.rb
@@ -9,10 +9,14 @@ class City < ActiveRecord::Base
   validates :state,     :presence => true
 
   scope :order_by_state, order('cities.state').order('cities.name')
+  scope :without_national_offer, where("cities.name <> ?", "Oferta Nacional")
+  scope :get_national_offer, where("cities.name = ?", "Oferta Nacional")
 
   def self.hash_by_states
     hash_of_states = {}
-    cities = City.order_by_state
+    cities = City.order_by_state.without_national_offer
+    national = City.get_national_offer
+    hash_of_states = {"Oferta Nacional" => [[national[0].name, national[0].id]]}
     last_state = cities[0].state
     cities_of_last_state = []
     cities.each do |c|
