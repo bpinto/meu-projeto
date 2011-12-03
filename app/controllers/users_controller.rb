cd6c8948fb999d29a769a7bc6fcfe903f8e2be1c
jplethier <jplethier@gmail.com>
2011-12-03 17:21:00 -0200

    redirecionando para a página que chamou a acao de follow ou unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # coding: UTF-8
 class UsersController < ApplicationController
   before_filter :find_user_with_deals, :only => :show
+  before_filter :store_location, :only => [:index, :show]
   prepend_before_filter :find_users, :only => :index
   # before_filter :authenticate_user!
 
@@ -24,19 +25,25 @@ class UsersController < ApplicationController
   def follow
     unless current_user.follow? @user
       current_user.follow! @user
-      redirect_to user_path(@user.username), :notice => I18n.t('models.user.started_following', :username => @user.username)
+      redirect_to stored_location, :notice => I18n.t('models.user.started_following', :username => @user.username)
+      #redirect_to user_path(@user.username), :notice => I18n.t('models.user.started_following', :username => @user.username)
     else
-      redirect_to user_path(@user.username), :alert => I18n.t('models.user.already_following', :username => @user.username)
+      redirect_to stored_location, :alert => I18n.t('models.user.already_following', :username => @user.username)
+      #redirect_to user_path(@user.username), :alert => I18n.t('models.user.already_following', :username => @user.username)
     end
+    clear_stored_location
   end
 
   def unfollow
     if current_user.follow? @user
       current_user.unfollow! @user
-      redirect_to user_path(@user.username), :notice => I18n.t('models.user.stopped_following', :username => @user.username)
+      redirect_to stored_location, :notice => I18n.t('models.user.stopped_following', :username => @user.username)
+      #redirect_to user_path(@user.username), :notice => I18n.t('models.user.stopped_following', :username => @user.username)
     else
-      redirect_to user_path(@user.username), :alert => I18n.t('models.user.not_following', :username => @user.username)
+      redirect_to stored_location, :alert => I18n.t('models.user.not_following', :username => @user.username)
+      #redirect_to user_path(@user.username), :alert => I18n.t('models.user.not_following', :username => @user.username)
     end
+    clear_stored_location
   end
 
   private
@@ -48,6 +55,18 @@ class UsersController < ApplicationController
   def find_users
     @users = User.paginate(:page => params[:page])
     @users = @users.search(params[:search].gsub(" ", "%")) if params[:search]
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def stored_location
+    session[:return_to]
+  end
+
+  def clear_stored_location
+    session[:return_to] = nil
   end
 
 end
